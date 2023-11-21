3887ed4d95d5cf4484b4f315b67c7fa98969427f
davi.silva.3.glo <davi.silva.3@globalhitss.com.br>
2023-11-21 12:18:40 -0300

    Adicionando primeiras componentes

---
+++ src/components/FormCadastro.jsx
@@ -0,0 +1,14 @@
+import { React, Component } from "react";
+
+export class FormCadastro extends Component {
+    render() {
+        return (
+            <form>
+                <input type="text" placeholder="Título" />
+                <textarea placeholder="Escreva sua nota..." />
+                <button>Criar Nota</button>
+            </form>
+        )
+    }
+}
+
