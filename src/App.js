0d36de31abcffcd294e7eaf69ec30b72f58fb639
davi.silva.3.glo <davi.silva.3@globalhitss.com.br>
2023-11-22 10:42:18 -0300

    Finalizando a primeira parte do projeto Ceep

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import { FormCadastro } from "./components/FormCadastro";
-import { ListasDeNotas } from "./components/ListaDeNotas";
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
+class App extends Component {
 
-function App() {
-  return (
-    <section>
-      <FormCadastro/>
-      <ListasDeNotas/>
-    </section>
-  );
+  constructor(){
+    super();
+    this.state = {
+      notas: []
+    };
+
+  }
+
+  criarNota(titulo, texto){
+    const novaNota = { titulo, texto}
+    const novoArrayNotas = [ ...this.state.notas, novaNota ]
+    const novoEstado = {
+      notas: novoArrayNotas,
+    }
+    
+    this.setState(novoEstado);
+  }
+
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <ListaDeNotas notas={this.state.notas}/>
+      </section>
+    );
+  }
 }
 
 export default App;
